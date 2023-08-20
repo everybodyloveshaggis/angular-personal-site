94a19a162a92768dca0ce5432a3fd4199ae3e3bf
Scott McMahon <everybodyloveshaggis@gmail.com>
2023-08-20 02:57:10 +0100

    eod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,17 @@ import { AppComponent } from './app.component';
 import { SummaryComponent } from './summary/summary.component';
 import { PersonalExperienceComponent } from './personal-experience/personal-experience.component';
 import { ProfessionalExperienceComponent } from './professional-experience/professional-experience.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent
-  , SummaryComponent, PersonalExperienceComponent, ProfessionalExperienceComponent, ProfessionalExperienceComponent],
+  , SummaryComponent, PersonalExperienceComponent, ProfessionalExperienceComponent, ProfessionalExperienceComponent, NavBarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
-, exports: [SummaryComponent, PersonalExperienceComponent, ProfessionalExperienceComponent]})
+, exports: [SummaryComponent, PersonalExperienceComponent, ProfessionalExperienceComponent, NavBarComponent]})
 export class AppModule { }
