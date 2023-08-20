edbe7512069ae2f9cf47e28055993c42510c9513
Scott McMahon <everybodyloveshaggis@gmail.com>
2023-08-20 01:36:34 +0100

    eod commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SummaryComponent } from './summary/summary.component';
+import { PersonalExperienceComponent } from './personal-experience/personal-experience.component';
+import { ProfessionalExperienceComponent } from './professional-experience/professional-experience.component';
 
 
 /*
 Routing
 */
 const routes: Routes = [
-  {    path: 'home',                        component:SummaryComponent,     title:'Scott McMahon'  },
-  {    path: 'personal-experience',         component:SummaryComponent,     title:'Scott M: Personal Experience'  },
-  {    path: 'professional-experience',     component:SummaryComponent,     title:'Scott M: Professional Experience'  },
-  {    path: 'working-with-disabilities',   component:SummaryComponent,     title:'Scott M: Working With Disabilities'  },
-  {    path: 'contact-me',                  component:SummaryComponent,     title:'Scott M: Contact Me!'  }
+  {    path: 'home',                        component:SummaryComponent,                     title:'Scott McMahon'  },
+  {    path: 'personal-experience',         component:PersonalExperienceComponent,          title:'Scott M: Personal Experience'  },
+  {    path: 'professional-experience',     component:ProfessionalExperienceComponent,      title:'Scott M: Professional Experience'  },
+  {    path: 'working-with-disabilities',   component:SummaryComponent,                     title:'Scott M: Working With Disabilities'  },
+  {    path: 'contact-me',                  component:SummaryComponent,                     title:'Scott M: Contact Me!'  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SummaryComponent } from './summary/summary.component';
 import { PersonalExperienceComponent } from './personal-experience/personal-experience.component';
+import { ProfessionalExperienceComponent } from './professional-experience/professional-experience.component';
 
 @NgModule({
   declarations: [
     AppComponent
-  , SummaryComponent, PersonalExperienceComponent],
+  , SummaryComponent, PersonalExperienceComponent, ProfessionalExperienceComponent, ProfessionalExperienceComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
-, exports: [SummaryComponent, PersonalExperienceComponent]})
+, exports: [SummaryComponent, PersonalExperienceComponent, ProfessionalExperienceComponent]})
 export class AppModule { }
